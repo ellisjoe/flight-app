67fb7072da64badf0709f593eba0ff0fd4a790c8
Joe Ellis <ellis125@gmail.com>
2025-12-31 12:42:59 -0700

    Try memoizing

--- app-2/src/App.tsx
+++ app-2/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
@@ -12,7 +12,7 @@ const DENVER: LatLngExpression = [39.76, -105.08];
 const createPlaneIcon = (rotation: number): DivIcon => {
   return L.divIcon({
     html: `<img src="${planeSvg}" style="width: 30px; height: 30px; transform: rotate(${rotation}deg);" />`,
-    className: 'plane-icon',
+    className: "plane-icon",
     iconSize: [30, 30],
     iconAnchor: [15, 15],
   });
@@ -22,10 +22,10 @@ function App() {
   const [time, setTime] = useState<Date>();
   const [planes, setPlanes] = useState<Plane[]>([]);
 
-  const fetchUsers = async () => {
+  const fetchPlanes = async () => {
     try {
       const endDate = time == null ? new Date() : time;
-      const startDate = new Date(endDate.getTime() - 600_000)
+      const startDate = new Date(endDate.getTime() - 600_000);
 
       const response = await fetch("http://localhost:3000/planes", {
         method: "POST",
@@ -33,7 +33,7 @@ function App() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          // start: startDate.toISOString(),
+          start: startDate.toISOString(),
           end: endDate.toISOString(),
         }),
       });
@@ -42,7 +42,7 @@ function App() {
       }
       const data: Plane[] = await response.json();
 
-      console.log(planes);
+      console.log(data);
 
       setPlanes(data);
     } catch (err: any) {
@@ -51,14 +51,18 @@ function App() {
   };
 
   useEffect(() => {
-    fetchUsers();
+    fetchPlanes();
 
     const intervalId = setInterval(() => {
-      fetchUsers();
-    }, 1000); // 1000 milliseconds = 1 second
+      fetchPlanes();
+    }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
+
+  const markers = useMemo(() => {
+    return planes.map((p) => <MapMarker {...p} key={p.hex_ident} />);
+  }, [planes]);
 
   return (
     <div className="App">
@@ -68,18 +72,59 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {planes.map((p) => (
-            <Marker key={p.hex_ident} position={[p.latitude, p.longitude]} icon={createPlaneIcon(p.track)}>
-              <Popup>
-                {p.callsign}
-              </Popup>
-            </Marker>
-          ))}
+          {markers}
         </MapContainer>
       </header>
     </div>
   );
 }
+
+const MapMarker = React.memo(
+  ({
+    latitude,
+    longitude,
+    callsign,
+    hex_ident,
+    track,
+  }: {
+    latitude: number;
+    longitude: number;
+    track: number;
+    callsign: string;
+    hex_ident: string;
+  }) => {
+    // function MapMarker({p}: {p: Plane}) {
+    const position: any = useMemo(
+      () => [latitude, longitude],
+      [latitude, longitude]
+    );
+    const icon = useMemo(() => createPlaneIcon(track), [track]);
+
+    console.log("rending map marker");
+
+    return (
+      <Marker position={position} icon={icon}>
+        <MyPopup callsign={callsign} hex_ident={hex_ident}></MyPopup>
+        {/* <Popup autoClose={false}>
+        <div>Callsign: {callsign}</div>
+        <div>Hex Ident: {hex_ident}</div>
+      </Popup> */}
+      </Marker>
+    );
+  }
+);
+
+const MyPopup = React.memo(
+  ({ callsign, hex_ident }: { callsign: string; hex_ident: string }) => {
+    console.log("rendering popup");
+    return (
+      <Popup autoClose={false}>
+        <div>Callsign: {callsign}</div>
+        <div>Hex Ident: {hex_ident}</div>
+      </Popup>
+    );
+  }
+);
 
 interface Plane {
   aircraft_id: string;
